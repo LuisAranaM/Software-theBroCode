0b72e85227f66ff126743123e48de075356153db
Luis Arana Motta <luis.arana@pucp.pe>
2018-09-02 20:34:58 -0500

    favicon

--- resources/views/Layouts/layout.blade.php
+++ resources/views/Layouts/layout.blade.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}" />
-  <link rel="shortcut icon" href="img/favicon.ico">
+  <link rel="icon" href="img/barney.jpg">       
 
   <title>@yield('pageTitle') - theBroCode</title>
  
--- resources/views/Layouts/layoutLogin.blade.php
+++ resources/views/Layouts/layoutLogin.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" href="img/favicon.ico">
+        <link rel="icon" href="img/barney.jpg">
         <title>@yield('pageTitle') - theBroCode</title>
 
         <!-- CSS-->
