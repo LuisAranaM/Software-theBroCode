db17250f481c27bcd821a8e3a4d574dc966f3ab6
Daniela <danielaa.296@hotmail.com>
2018-09-26 19:14:52 -0500

    CambiosDaniela7:14

--- resources/views/cursos/horarios.blade.php
+++ resources/views/cursos/horarios.blade.php
@@ -31,14 +31,14 @@
 <div class="row">
   <div class=" x_panel tile coursesBox">
     <div class="row">
-    <div class="col-xs-6" >
-      <h1 class="secondaryTitle mainTitle" style="text-align: left">Gestionar Horarios </h1>
-    </div>
+      <div class="col-xs-6" >
+        <h1 class="secondaryTitle mainTitle" style="text-align: left">Gestionar Horarios </h1>
+      </div>
 
-    <div class="col-sm-6 col-xs-6" style="text-align: right">
-      <button type="button" class="btn btn-success btn-lg pText customButton" style="width: 120px">Cargar Alumnos  </button>
-    </div>  
-  </div>
+      <div class="col-sm-6 col-xs-6" style="text-align: right">
+        <button type="button" class="btn btn-success btn-lg pText customButton" style="width: 120px">Cargar Alumnos  </button>
+      </div>  
+    </div>
 
     <div class="x_content bs-example-popovers courseContainer">
       <div class="alert alert-success alert-dismissible fade in courseButton" role="alert" style="background-color: white; color: #00626e; border-color: #c1e2fc;">
@@ -76,118 +76,77 @@
 
 <div class="row">
   <div class=" x_panel tile coursesBox">
-
     <div class="col-xs-12" >
       <h1 class="secondaryTitle mainTitle" style="text-align: left">Resultados del Estudiantes / Indicadores de Desempeño</h1>
     </div>
-    <div class="row">
-      <div class="col-xs-2" >
-        <div class="x_content bs-example-popovers">
-          <div class="alert alert-success alert-dismissible fade in courseButton" role="alert" style="background-color: #d3eafd; color: #00626e; border-color: #c1e2fc;">
-            <button type="button" class="close " data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
-            </button>
-            <p class="pText"> A</p>
-          </div>
-        </div>
+
+    <!--Criterio 1-->
+    <div class="row" style="margin-bottom: 30px">
+      <div class="col-md-3 pText">
+        Criterio A: Matemáticas
       </div>
-
-      <div class="col-xs-2" >
-        <div class="x_content bs-example-popovers">
-          <div class="alert alert-success alert-dismissible fade in courseButton" role="alert" style="background-color: #d3eafd; color: #00626e; border-color: #c1e2fc">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
-            </button>
-            <p class="pText">A1</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-xs-2" >
-        <div class="x_content bs-example-popovers">
-          <div class="courseButton alert alert-success alert-dismissible fade in " role="alert" style="background-color: #d3eafd; color: #00626e; border-color: #c1e2fc">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
-            </button>
-            <p class="pText"> A2 </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-xs-2">
-        <div class="x_content bs-example-popovers">
-          <div class="alert alert-success alert-dismissible fade in courseButton" role="alert" style="background-color: white; color: #00626e; border-color: #c1e2fc;">
-            <button type="button" class="close" aria-label="Close"><span aria-hidden="true">+</span>
-            </button>
-            <p class="pText"> Nuevo Criterio </p>
-          </div>
-        </div>
-      </div>
-
-    </div>
-
-
-    <div class="row">
-      <div class="col-xs-2" >
-        <div class="x_content bs-example-popovers">
-          <div class="alert alert-success alert-dismissible fade in courseButton" role="alert" style="background-color: #d3eafd; color: #00626e; border-color: #c1e2fc;">
-            <button type="button" class="close " data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
-            </button>
-            <p class="pText"> B</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-xs-2" >
-        <div class="x_content bs-example-popovers">
-          <div class="alert alert-success alert-dismissible fade in courseButton" role="alert" style="background-color: #d3eafd; color: #00626e; border-color: #c1e2fc">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
-            </button>
-            <p class="pText">B1 </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-xs-2" >
-        <div class="x_content bs-example-popovers">
-          <div class="courseButton alert alert-success alert-dismissible fade in " role="alert" style="background-color: #d3eafd; color: #00626e; border-color: #c1e2fc">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
-            </button>
-            <p class="pText"> B2 </p>
-          </div>
-        </div>
+      <div class="col-md-9">
+        <div class="btn-group btn-group-justified" data-toggle="buttons" >
+          <label class="btn btn-primary active"  style="background-color: #00626E; border-color: #004d54">
+            <input type="radio" class="sr-only" id="viewMode1" name="viewMode" value="1">
+            <span class="docs-tooltip" data-toggle="tooltip" title="View Mode 1">
+              B
+            </span>
+          </label>
+          <label class="btn btn-primary" style="background-color: #00626E; border-color: #004d54">
+            <input type="radio" class="sr-only" id="viewMode1" name="viewMode" value="1">
+            <span class="docs-tooltip" data-toggle="tooltip" title="View Mode 1">
+              B2
+            </span>
+          </label>
+          <label class="btn btn-primary" style="background-color: #00626E; border-color: #004d54">
+            <input type="radio" class="sr-only" id="viewMode1" name="viewMode" value="1">
+            <span class="docs-tooltip" data-toggle="tooltip" title="View Mode 1">
+              B2
+            </span>
+          </label>
+        </label>
+        <label class="btn btn-primary" style="background-color: #00626E; border-color: #004d54; width: 1px">
+          <input type="radio" class="sr-only" id="viewMode1" name="viewMode" value="1">
+          <span class="docs-tooltip" data-toggle="tooltip" title="View Mode 1">
+            +
+          </span>
+        </label>
       </div>
     </div>
-
-    <div class="row">
-      <div class="col-xs-2" >
-        <div class="x_content bs-example-popovers">
-          <div class="alert alert-success alert-dismissible fade in courseButton" role="alert" style="background-color: #d3eafd; color: #00626e; border-color: #c1e2fc;">
-            <button type="button" class="close " data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
-            </button>
-            <p class="pText"> C</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-xs-2" >
-        <div class="x_content bs-example-popovers">
-          <div class="alert alert-success alert-dismissible fade in courseButton" role="alert" style="background-color: #d3eafd; color: #00626e; border-color: #c1e2fc">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
-            </button>
-            <p class="pText">C1 </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-xs-2" >
-        <div class="x_content bs-example-popovers">
-          <div class="courseButton alert alert-success alert-dismissible fade in " role="alert" style="background-color: #d3eafd; color: #00626e; border-color: #c1e2fc">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
-            </button>
-            <p class="pText"> C2 </p>
-          </div>
-        </div>
-      </div>
-    </div>
-
   </div>
+
+  <!--Criterio 2-->
+  <div class="row">
+    <div class="col-md-3 pText">
+      Criterio A: Matemáticas
+    </div>
+    <div class="col-md-9">
+      <div class="btn-group btn-group-justified" data-toggle="buttons">
+        <label class="btn btn-primary" style="background-color: #00626E; border-color: #004d54">
+          <input type="radio" class="sr-only" id="viewMode1" name="viewMode" value="1">
+          <span class="docs-tooltip" data-toggle="tooltip" title="View Mode 1">
+            B2
+          </span>
+        </label>
+        <label class="btn btn-primary" style="background-color: #00626E; border-color: #004d54">
+          <input type="radio" class="sr-only" id="viewMode1" name="viewMode" value="1">
+          <span class="docs-tooltip" data-toggle="tooltip" title="View Mode 1">
+            B2
+          </span>
+        </label>
+        <label class="btn btn-primary" style="background-color: #00626E; border-color: #004d54">
+          <input type="radio" class="sr-only" id="viewMode1" name="viewMode" value="1">
+          <span class="docs-tooltip" data-toggle="tooltip" title="View Mode 1">
+            B2
+          </span>
+        </label>
+      </div>
+    </div>
+  </div>
+
+
+</div>
 </div>
 
 
--- resources/views/cursos/reportes.blade.php
+++ resources/views/cursos/reportes.blade.php
@@ -12,53 +12,83 @@
 
   <div class="row">
 
-    <div class="col-xs-6">
+    <div class="col-md-6">
       <div class=" x_panel tile coursesBox">
         <div class="row">
           <div class="col-xs-6">
-           <img class= "imageBox" src="/img/report1.PNG" >
+           <img class= "imageBox" src="{{ URL::asset('img/report1.PNG') }}" >
          </div>
          <div class="col-xs-6 text-center">
-          <h1 class="reportsTitle mainTitle">Criterios x Ciclo </h1>
+          <h1 class="reportsTitle mainTitle">Resultados x Ciclo </h1>
+
+          <div class="row">
+            <div class="groupBoxOptions">
+            <div class="form-check">
+              <label>
+                <input type="checkbox" checked=""> <span class="pText label-text ">Comparar con semestre anterior</span>
+              </label>
+            </div>
+            </div>
+            <button type="button" class="btn btn-success btn-lg pText customButton" style="width: 120px">Generar Gráfico  </button>
+          </div>
         </div>
       </div>
     </div>
   </div>
 
-  <div class="col-xs-6">
+  <div class="col-md-6">
     <div class=" x_panel tile coursesBox">
       <div class="row">
         <div class="col-xs-6">
-         <img class= "imageBox" src="/img/report1.PNG" >
+         <img class= "imageBox" src="{{ URL::asset('img/report1.PNG') }}">
        </div>
        <div class="col-xs-6 text-center">
-        <h1 class="reportsTitle mainTitle">Cursos x Criterio </h1>
+        <h1 class="reportsTitle mainTitle">Cursos x Resultado </h1>
+
+        <div class="row">
+            <div class="groupBoxOptions">
+            <div class="form-check">
+              <label>
+                <input type="checkbox" checked=""> <span class="pText label-text">Comparar con semestre anterior</span>
+              </label>
+            </div>
+            </div>
+            <button type="button" class="btn btn-success btn-lg pText customButton" style="width: 120px">Generar Gráfico  </button>
+        </div>
       </div>
     </div>
   </div>
 </div>
 
 <div class="row">
-  <div class="col-xs-6">
+  <div class="col-md-6">
     <div class=" x_panel tile coursesBox">
       <div class="row">
         <div class="col-xs-6">
-         <img class= "imageBox" src="/img/report1.PNG" >
+         <img class= "imageBox" src= "{{ URL::asset('img/report1.PNG') }}" >
        </div>
        <div class="col-xs-6 text-center">
-        <h1 class="reportsTitle mainTitle">Subcriterios x Criterio </h1>
+        <h1 class="reportsTitle mainTitle">Indicadores x Resultado </h1>
+
+        <div class="row" style="padding-bottom: 20px; padding-top: 20px;">
+            <button type="button" class="btn btn-success btn-lg pText customButton" style="width: 120px">Generar Gráfico  </button>
+        </div>
       </div>
     </div>
   </div>
 </div>
-<div class="col-xs-6">
+<div class="col-md-6">
   <div class=" x_panel tile coursesBox">
     <div class="row">
       <div class="col-xs-6">
-       <img class= "imageBox" src="/img/report1.PNG" >
+       <img class= "imageBox" src="{{ URL::asset('img/report1.PNG') }}" >
      </div>
      <div class="col-xs-6 text-center">
       <h1 class="reportsTitle mainTitle">Consolidado Histórico </h1>
+      
+        <div class="row" style="padding-bottom: 20px; padding-top: 20px;">
+            <button type="button" class="btn btn-success btn-lg pText customButton" style="width: 120px">Generar Gráfico  </button>
+        </div>
     </div>
   </div>
 </div>
