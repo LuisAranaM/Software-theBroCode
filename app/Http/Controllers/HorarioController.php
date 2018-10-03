66bd3abc24814d633ef66c5c047f70730e3a1ab1
Luis Arana Motta <luis.arana@pucp.pe>
2018-10-03 11:42:05 -0500

    fix Horarios

--- app/Http/Controllers/HorarioController.php
+++ app/Http/Controllers/HorarioController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Entity\PruebaEntity as Horario;
+use App\Entity\Horario as Horario;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
