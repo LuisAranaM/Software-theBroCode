ef9d82b78f42e3974fedf4ddd695eac7c38ad1c8
Luis Arana Motta <luis.arana@pucp.pe>
2018-10-03 02:06:48 -0500

    Entity Curso

---
+++ app/Entity/Curso.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Entity;
+
+use \Illuminate\Pagination\LengthAwarePaginator as Paginator;
+use App\Models\Curso as mCurso;
+use Jenssegers\Date\Date as Carbon;
+
+class Curso extends \App\Entity\Base\Entity {
+
+	protected $_fechaRegistro;
+    
+    function setProperties($data) {
+        $this->setValues([
+            '_fechaRegistro' => $data->FECHA_REGISTRO,
+        ]);
+    }
+
+    function setValueToTable() {
+        return $this->cleanArray([
+            'FECHA_REGISTRO' => $this->_fechaRegistro,
+        ]);
+    }
+
+    static function getCursos() {
+        $model = new mCurso();
+        return mCurso::getCursos()->get();
+    }
+
+}
