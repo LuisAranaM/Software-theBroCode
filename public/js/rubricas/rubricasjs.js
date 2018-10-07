8a8f9d26ae11a521c496bfade88d7c116c1897e9
Daniela <danielaa.296@hotmail.com>
2018-10-07 15:53:04 -0500

    Rubricas Daniela

--- public/js/rubricas/rubricasjs.js
+++ public/js/rubricas/rubricasjs.js
@@ -4,5 +4,8 @@ $( document ).ready(function() {
 	.css('height', 'auto')
 	.find('.collapse-link i').toggleClass('fa-chevron-up fa-chevron-down').end()
 	.find('.x_content').css('display', 'none');
+
+    $("#hola").click(function () {
+        $("#hola").hide();
 });
 
