6cd86a0065b5715b66d8cbd5cf6bc3016e7ab48e
Hykire <andpando20@gmail.com>
2018-10-09 23:27:05 -0500

    karla, ya deje todo rdy con comentarios para que lo pongas pls :3

--- app/Http/Controllers/ProyectoController.php
+++ app/Http/Controllers/ProyectoController.php
@@ -16,7 +16,7 @@ class ProyectoController extends Controller
 
     public function store(Request $request){
         $codigo = $request->get('codAlumno', null);
-        #dd($codigo);
+        $horario = $request->get('horario', null);
     	$file = $request->file('archivo');
     	$semestre_actual = Entity::getIdSemestre();
     	$especialidad = Entity::getEspecialidadUsuario();
@@ -30,14 +30,22 @@ class ProyectoController extends Controller
 
     	#creationg array for data
     	$data = array('RUTA'=>$path, 'FECHA_REGISTRO'=>$fecha, 'FECHA_ACTUALIZACION'=>$fecha, 'USUARIO_MODIF'=>$id_usuario,'ESTADO'=>1, 'NOMBRE'=>$name_of_file.'.'.$extension_of_file);
-        $idProyectos = DB::table('PROYECTOS')->insertGetId(
+        $idProyecto = DB::table('PROYECTOS')->insertGetId(
             $data
         );
         $idAlumno = DB::table('ALUMNOS')
                      ->select(DB::raw('ID_ALUMNO'))
                      ->where('CODIGO', '=', $codigo)
                      ->get();
-        dd($idAlumno[0]->ID_ALUMNO);
+        $idHorario = DB::table('HORARIO')
+                     ->select(DB::raw('ID_HORARIO'))
+                     ->where('NOMBRE', '=', $horario)
+                     ->get();
+        #ahora insertaré en alumnos_has_horarios los atributos correspondientes para anexar el file subido al alumno en el horario respectivo
+        $dataAlumnoxHorario = array('ID_ALUMNO'=>$idAlumno[0]->ID_ALUMNO, 'ID_HORARIO'=>$idHorario[0]->ID_HORARIO, 'ID_PROYECTO'=>$idProyecto, 'SEMESTRES_ID_SEMESTRE'=>$id_usuario,'FECHA_REGISTRO'=>$fecha, 'FECHA_ACTUALIZACION'=>$fecha, 'USUARIO_MODIF'=>$id_usuario,'ESTADO'=>1);
+        $idAlumnoHasHorarios = DB::table('ALUMNOS_HAS_HORARIOS')->insertGetId(
+            $dataAlumnoxHorario
+        );
     	flash('Se ha subido el archivo de forma correcta.')->success();
     	return back();
     }
