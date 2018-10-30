5ed84d4329bc534c2d09e8a4832e49a78e84438e
Daniela <danielaa.296@hotmail.com>
2018-10-30 10:52:25 -0500

    Rubricas Daniela

--- resources/views/rubricas/categorias.blade.php
+++ resources/views/rubricas/categorias.blade.php
@@ -9,118 +9,45 @@
 
 	<div class="row">
 		<div class="col-md-8 col-sm-6">
-			<h1 class="mainTitle" ><a href="{{route('rubricas.gestion')}}">Lista de Resultados</a> > <a>Categorías del Resultado A</a></h1>
-		</div>
-
-		<div class="col-md-4 col-sm-6 form-group top_search" >
-			<div class="input-group">
-				<input type="text" class="form-control searchText" placeholder="Curso...">
-				<span class="input-group-btn">
-					<button class="btn btn-default searchButton" type="button">Buscar</button>
-				</span>
-			</div>
+			<h1 class="mainTitle" ><a href="{{route('rubricas.gestion')}}"> Categorias del Resultado {{$resultado}}</a></h1>
 		</div>
 	</div>
-
 	<div class="row">
+		@foreach ($categorias as $categoria) 
 		<div class="col-md-4 col-xs-6">
 			<div class="x_panel tile coursesBox">
-				<!-- CURSOS CARGADOS DE LA BD -->
+				<!-- CATEGORIAS CARGADOS DE LA BD -->
 				<div class="row rowFinal" style="padding-bottom: 0px">
 					<div class="row">
-						<h1 class="secondaryTitle mainTitle">Matemáticas</h1>
-					</div>
-
-					<hr>
+						<h1 class="secondaryTitle mainTitle">{{$categoria->NOMBRE}}</h1>
+					</div>					
+					<?php $count = 1; ?>
+					@foreach ($indicadoresTodos[$categoria->ID_CATEGORIA] as $indicador) 
+					
 					<div class="row">
+						<hr>
 						<div class="col-xs-9">
-							<p class="pText" style="font-weight: bold; color: black">A.1.</p>
+							<p class="pText" style="font-weight: bold; color: black">{{$resultado}}.{{$count}}</p>
 						</div>
 						<div class="col-xs-3" style="text-align: right">
-							<i class="fa fa-pencil fa-lg" style="color: #005b7f"></i>
-							<i class="fa fa-trash fa-lg" style="color: #005b7f; padding-left: 2px"></i>
+							<i class="edit fa fa-pencil fa-lg" style="color: #005b7f" id ="EditarIndicador"></i>
+							<i class="fa fa-trash fa-lg" style="color: #005b7f; padding-left: 2px; cursor: pointer"></i>
 						</div>
 						<div class="col-xs-12">
-							<p class="pText">(*) Aplica conceptos lógicos para la resolucion de problemas</p>
+							<p class="pText">{{$indicador->NOMBRE}}</p>
 						</div>
 					</div>
-					<hr>
-					<div class="row">
-						<div class="col-xs-9">
-							<p class="pText" style="font-weight: bold; color: black">A.2.</p>
-
-						</div>
-						<div class="col-xs-3" style="text-align: right">
-							<i class="fa fa-pencil fa-lg" style="color: #005b7f"></i>
-							<i class="fa fa-trash fa-lg" style="color: #005b7f; padding-left: 2px"></i>
-						</div>
-						<div class="col-xs-12">
-							<p class="pText">Diseña algoritmos para la resolución de un problema identificado</p>
-						</div>
-					</div>
-					<hr>
-					<div class="row">
-						<div class="col-xs-9">
-							<p class="pText" style="font-weight: bold; color: black">A.3.</p>
-						</div>
-						<div class="col-xs-3" style="text-align: right">
-							<i class="fa fa-pencil fa-lg" style="color: #005b7f"></i>
-							<i class="fa fa-trash fa-lg" style="color: #005b7f; padding-left: 2px"></i>
-						</div>
-						<div class="col-xs-12">
-							<p class="pText">Utiliza lenguajes de programación para implementar algoritmos sean diseñados por él o por cualquier otra persona</p>
-						</div>
-
-					</div>
+					<?php $count = $count +1; ?>
+					@endforeach					
 					<hr>
 					<div class="row text-center">
-						<p class="pText"  id ="AgregarIndicador" style="color: #005b7f">Agregar nuevo indicador</p>
+						<p class="pText agregarIndicador" style="color: #005b7f">Agregar nuevo indicador</p>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="col-md-4 col-xs-6">
-			<div class="x_panel tile coursesBox">
-				<!-- CURSOS CARGADOS DE LA BD -->
-				<div class="row rowFinal" style="padding-bottom: 0px">
-					<div class="row">
-						<h1 class="secondaryTitle mainTitle">Ingeniería Informática</h1>
-					</div>
-
-					<hr>
-					<div class="row">
-						<div class="col-xs-9">
-							<p class="pText" style="font-weight: bold; color: black">A.1.</p>
-						</div>
-						<div class="col-xs-3" style="text-align: right">
-							<i class="fa fa-pencil fa-lg" style="color: #005b7f"></i>
-							<i class="fa fa-trash fa-lg" style="color: #005b7f; padding-left: 2px"></i>
-						</div>
-						<div class="col-xs-12">
-							<p class="pText">(*) Aplica conceptos lógicos para la resolucion de problemas</p>
-						</div>
-					</div>
-					<hr>
-					<div class="row">
-						<div class="col-xs-9">
-							<p class="pText" style="font-weight: bold; color: black">A.2.</p>
-
-						</div>
-						<div class="col-xs-3" style="text-align: right">
-							<i class="fa fa-pencil fa-lg" style="color: #005b7f"></i>
-							<i class="fa fa-trash fa-lg" style="color: #005b7f; padding-left: 2px"></i>
-						</div>
-						<div class="col-xs-12">
-							<p class="pText">Diseña algoritmos para la resolución de un problema identificado</p>
-						</div>
-					</div>
-					<hr>
-					<div class="row text-center">
-						<p class="pText"  id ="CargarCurso" style="color: #005b7f">Agregar nuevo indicador</p>
-					</div>
-				</div>
-			</div>
-		</div>
+		@endforeach
+		
 	</div>
 	<!-- END CURSOS CARGADOS DE LA BD-->     
 
@@ -139,7 +66,9 @@ aria-labelledby="gdridfrmnuavaUO" data-focus-on="input:first">
 			aria-label="Close">
 			<span aria-hidden="true">&times;</span>
 		</button>
-		<h4 id="gridSystemModalLabel" class="reportsTitle mainTitle modal-title" style="padding-top: 10px" id="gridSystemModalLabel">Agregar Nuevo Indicador</h4>
+
+		<label  class="reportsTitle mainTitle modal-title" style="padding-top: 10px" id="ModalTitle" name="codigoHorario" type="text" value=""></label>
+		
 	</div>
 	<hr style="padding: 0px; margin-top: 0px; margin-bottom: 0px; width: 80%">
 
@@ -162,20 +91,18 @@ aria-labelledby="gdridfrmnuavaUO" data-focus-on="input:first">
 						<div class="col-xs-12" style="padding-bottom: 6px">
 
 							<textarea type="text" id="txtCategoria" class="form-control pText customInput" name="nombre" placeholder="Código de valorización" rows="1" cols="30" style="resize: none;" ></textarea>       
+
 						</div>
 
 						<div class="col-xs-12">
 							<textarea type="text" id="txtCategoria" class="form-control pText customInput" name="nombre" placeholder="Descripción de la valorización" rows="3" cols="30" style="resize: none;" ></textarea>       
 						</div>
-						<div class="row">
-							<div class="col-xs-12 text-left" style="padding-left: 22px; padding-top: 10px">
-								<p class="pText">Agregar nueva valorización <i class="fa fa-plus-circle fa-2x" style="color: #005b7f; padding-top: 2px; padding-left: 4px"></i></p>
-							</div>
-					
+						<div class="col-lg-6 col-xs-5 text-left" style="padding-top: 15px">
+							<p class="pText">Agregar nueva valorización</p>
 						</div>
-
-
-
+						<div class="col-md-2 col-sm-2 text-left" style="padding-top: 10px; margin-left: -40px">
+							<i class="fa fa-plus-circle fa-2x" style="color: #005b7f; padding-top: 2px"></i>
+						</div>				
 					</div>
 				</div>
 
