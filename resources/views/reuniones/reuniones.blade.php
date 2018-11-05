f3787cdc0f331c04fd64945b3e7765d31dc6d1d1
YoluG <yoluanags@gmail.com>
2018-11-05 00:04:14 -0500

    Modal y vista Reuniones

--- resources/views/reuniones/reuniones.blade.php
+++ resources/views/reuniones/reuniones.blade.php
@@ -2,7 +2,7 @@
 @section('pageTitle', 'Principal')
 @section('content')
 @section('js-libs')
-<script type="text/javascript"  src="{{ URL::asset('js/avisos/avisos.js') }}"></script>
+<script type="text/javascript"  src="{{ URL::asset('js/reuniones/reuniones.js') }}"></script>
 @stop
 
 <div class="customBody">
@@ -18,13 +18,13 @@
 	<div class="row">
 		<div class=" x_panel tile coursesBox">
 			
-			<div class="row x_panel" >
+			<div class="row x_panel" style="text-align: center;" >
 				<div class="col-md-2">
 					<label class="pText">Semestre inicio:</label>
 				</div>
 
 				<div class="col-md-2">
-					<input type="text" id="txt" class="form-control pText customInput" 
+					<input type="text" id="txt" class="form-control pText customInputDocsReuniones"  style="width: 70px;" 
 					name="semestreInicio" placeholder="" value="">  
 				</div>
 				
@@ -40,7 +40,7 @@
 				</div>
 
 				<div class="col-md-2">
-					<input type="text" id="txt" class="form-control pText customInput" 
+					<input type="text" id="txt" class="form-control pText customInputDocsReuniones"   style="width: 70px;"
 					name="semestreFin" placeholder="" value="">  
 				</div>
 
@@ -77,6 +77,24 @@
 									<form>
 
 										<td class="pText" style="background-color: white; padding-top: 12px; color: #72777a;text-align: center;vertical-align: center;">2017-1</td>
+										<td class="pText" style="background-color: white; padding-top: 12px; color: #72777a;text-align: center;vertical-align: center;">Acta de Reunón</td>
+										<td><input type="checkbox" name="select"></td>
+									</form>
+
+								</tr>
+								<tr class="even pointer" id="">
+									<form>
+
+										<td class="pText" style="background-color: white; padding-top: 12px; color: #72777a;text-align: center;vertical-align: center;">2017-1</td>
+										<td class="pText" style="background-color: white; padding-top: 12px; color: #72777a;text-align: center;vertical-align: center;">Plan de Mejora</td>
+										<td><input type="checkbox" name="select"></td>
+									</form>
+
+								</tr>
+								<tr class="even pointer" id="">
+									<form>
+
+										<td class="pText" style="background-color: white; padding-top: 12px; color: #72777a;text-align: center;vertical-align: center;">2018-1</td>
 										<td class="pText" style="background-color: white; padding-top: 12px; color: #72777a;text-align: center;vertical-align: center;">Plan de Mejora</td>
 										<td><input type="checkbox" name="select"></td>
 									</form>
@@ -88,7 +106,11 @@
 
 					</div>
 				</div>
-
+				<div class="col-md-4">
+					<div class=" x_panel tile coursesBox" id="ModalCargar" style="text-align: center; cursor: pointer;">
+						<h2 class="">Cargar Documentos</h2> <i class="fa fa-upload" style="font-size: 30px;"></i> 
+					</div>
+				</div>
 			</div>
 
 			<div class="row">
@@ -106,6 +128,76 @@
 
 	
 
+	<!-- Modal de Cargar Documentos -->
+
+	<div class="modal fade bs-example-modal-lg text-center" role="dialog" tabindex="-1"
+	id="modalCargarDocsReuniones" data-keyboard="false" data-backdrop="static"
+	aria-labelledby="gdridfrmnuavaUO" data-focus-on="input:first" >
+	<div class="customModal modal-dialog modal-lg ">
+		<div class="modal-content" style="top: 30%">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal"
+				aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+			</button>
+			<h4 id="CargarDocumentos" class="reportsTitle mainTitle modal-title" style="padding-top: 10px" id="gridSystemModalLabel">Cargar Documentos</h4>
+		</div>
+		<hr style="padding: 0px; margin-top: 0px; margin-bottom: 0px; width: 80%">
+		<div class="modal-body">
+
+			<div class="row" style="padding: 10px;">
+
+				<label class="pText">Seleccionar tipo de documento a subir:</label>
+				<select name="semIni">
+					<option value="acta">Acta de Reunión</option>
+					<option value="plan">Plan de Mejora</option>
+				</select>
+			</div>
+			<div class="row" style="padding: 10px;">
+				<div class="col-md-8">
+					<label class="pText">Seleccionar semestre de creación:</label>
+				</div>
+				<div class="col-md-2">
+					<input type="text" id="txt" class="form-control pText customInputDocsReunionesModal"  style="width: 70px;" 
+					name="semestre" placeholder="" value=""> 
+				</div>	
+
+				<div class="col-md-2">
+					<select name="semCreacion">
+						<option value="1">1</option>
+						<option value="2">2</option>
+					</select>
+				</div>	 
+
+			</div>
+
+			<div class="container-fluid text-center">
+				<div class="dropzone" style="min-height: 100px; height: 190px; width: 350px; border: 2px dashed #ccc; display: inline-block; background-color: white; margin-top: 10px; margin-bottom: 10px">
+					<i class="fa fa-5x fa-cloud-upload" style="color: #ccc; height: 100px; padding: 10px"></i>
+					<p class="pText">Arrastra y suelta un archivo <br> o <br> 
+
+						<form id="upload_form" action = "" method = "post" enctype = "multipart/form-data">
+							{{csrf_field()}}
+							<div class = "form-group">
+								<input type = "file" name = "upload-file" class="form-control image" style="border-color: white">
+							</div>
+							<div class="row" style="padding-top: 20px; text-align: center; display: flex;justify-content: center;">
+								<div class="col-md-4">
+									<input id="btnCargarHorariosModal" class = "btn btn-success pText customButtonThin upload-file" style="padding-right: 5px; padding-left: 5px;" type="submit" value = "Cargar" name="submit">
+								</div>
+							</div>
+
+						</form>
+
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- /.modal-content -->
+	</div>
+	<!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
 
 </div>
 
