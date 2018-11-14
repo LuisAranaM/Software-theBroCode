44d580a6fccc9211e5791b423fe721c313620c9b
karlangadas <karla.pedraza@pucp.edu.pe>
2018-11-14 18:05:24 -0500

    reporte js

--- public/js/reportes/reportes.js
+++ public/js/reportes/reportes.js
@@ -100,6 +100,21 @@ $( document ).ready(function() {
         $('#ciclos4 option').last().prop('selected',true);
         $("#modalConsolidado").modal("show")
     });
+
+    $('#btnDescargarGraficos1').click(function(event) {
+        // get size of report page
+        var reportPageHeight = 297;
+        var reportPageWidth = 210;
+        
+        var canvas = document.querySelector('#graficoResultadoxCiclo');
+        var dataURL = canvas.toDataURL();
+        var pdf = new jsPDF();
+        pdf.addImage(dataURL, 'PNG', 35, 50);
+        semestre = document.getElementById('ciclos3').options[document.getElementById('ciclos1').selectedIndex].text;
+        pdf.text('Resultados del Ciclo '+semestre, 70, 40)
+        pdf.save('Gráfico Resultados '+semestre+".pdf");
+      });
+
 });
 
 function callback(data) {   
@@ -425,3 +440,46 @@ function graficoResultadoxCicloClickEvent(evt, chartElement){
  function cambiarCursor(evt, chartElement) {
     event.target.style.cursor = chartElement[0] ? 'pointer' : 'default';
  }
+
+ $('#downloadPdf').click(function(event) {
+  // get size of report page
+  var reportPageHeight = $('#reportPage').innerHeight();
+  var reportPageWidth = $('#reportPage').innerWidth();
+  
+  // create a new canvas object that we will populate with all other canvas objects
+  var pdfCanvas = $('<canvas />').attr({
+    id: "canvaspdf",
+    width: reportPageWidth,
+    height: reportPageHeight
+  });
+  
+  // keep track canvas position
+  var pdfctx = $(pdfCanvas)[0].getContext('2d');
+  var pdfctxX = 0;
+  var pdfctxY = 0;
+  var buffer = 100;
+  
+  // for each chart.js chart
+  $("canvas").each(function(index) {
+    // get the chart height/width
+    var canvasHeight = $(this).innerHeight();
+    var canvasWidth = $(this).innerWidth();
+    
+    // draw the chart into the new canvas
+    pdfctx.drawImage($(this)[0], pdfctxX, pdfctxY, canvasWidth, canvasHeight);
+    pdfctxX += canvasWidth + buffer;
+    
+    // our report page is in a grid pattern so replicate that in the new canvas
+    if (index % 2 === 1) {
+      pdfctxX = 0;
+      pdfctxY += canvasHeight + buffer;
+    }
+  });
+  
+  // create new pdf and add our new canvas as an image
+  var pdf = new jsPDF('l', 'pt', [reportPageWidth, reportPageHeight]);
+  pdf.addImage($(pdfCanvas)[0], 'PNG', 0, 0);
+  
+  // download the pdf
+  pdf.save('filename.pdf');
+});
