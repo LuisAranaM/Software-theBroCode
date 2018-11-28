00a65df4fed5ac6252d51c3d59f1832c7db62c5f
Luis Arana Motta <luis.arana@pucp.pe>
2018-11-28 15:18:33 -0500

    ID_SEMESTRE

--- app/Entity/Base/Entity.php
+++ app/Entity/Base/Entity.php
@@ -88,8 +88,8 @@ class Entity {
 
     public static function getIdSemestre(){
         $model= new mSemestre();
-        //return $model->getIdSemestre();
-        return 2;//env('ID_SEMESTRE');
+        return $model->getIdSemestre();
+        //return 2;//env('ID_SEMESTRE');
     }
 
 
