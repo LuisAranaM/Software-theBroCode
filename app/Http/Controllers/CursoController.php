30933535eed083b1e4c968e391306cdd0fd1ef7c
eduardovp97 <eduardo.velarde@pucp.pe>
2018-12-07 16:51:15 -0500

    Bug

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -456,8 +456,6 @@ class CursoController extends Controller
     private function getApellidoPaterno($x){
         $ans = '';
         for($i = 0; $i < strlen($x); $i++){
-            $this->trace($x[$i]);
-            if($x[$i] == ',') $this->trace('COMA');
             if( ($x[$i] == ' ') || ($x[$i] == ',') ) break;
             $ans .= $x[$i];
         }
