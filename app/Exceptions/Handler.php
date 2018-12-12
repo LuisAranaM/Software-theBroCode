bcf0e58c5907e43adf04c2171736cd6646976ff9
Luis Arana Motta <luis.arana@pucp.pe>
2018-12-11 22:17:49 -0500

    lol

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,7 +46,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        parent::render($request, $exception);
         /*switch ($exception->getStatusCode()) {
             case 404:
                 return redirect()->route('notfound');
